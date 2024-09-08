54c7f3fc2de674b6510611649414f7fa4f1b6353
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-09-08 12:14:17 +0530

    first

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main() {
+	// var  a  int
+	// var  s string
+	// var hasAny bool
+
+	// s= "test"
+	// a=10
+	// hasAny=true
+
+	var n1,n2,n3 int = 1, 2, 4
+	// fmt.Println(s)
+	// fmt.Println(a)
+	// fmt.Println(hasAny)
+
+	fmt.Println(n1)
+	fmt.Println(n2)
+	fmt.Println(n3)
+}
